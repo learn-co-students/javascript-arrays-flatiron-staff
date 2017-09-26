948e1ce0c57133a5d623f4b40244d85c6d2712e3
Haoshuang Wang <haoshuang@flatironschool.com>
2017-09-26 00:18:34 +0000

    Done.

--- arrays.js
+++ arrays.js
@@ -17,3 +17,27 @@ function destructivelyAddElementToEndOfArray (array, element) {
   array.push(element)
   return array
 }
+
+function accessElementInArray (array, index) {
+  return array[index]
+}
+
+function destructivelyRemoveElementFromBeginningOfArray (array) {
+  array.shift()
+  return array
+}
+
+function removeElementFromBeginningOfArray (array) {
+  return array.slice(1)
+  // return array
+}
+
+function destructivelyRemoveElementFromEndOfArray (array) {
+  array.pop()
+  return array
+}
+
+function removeElementFromEndOfArray (array) {
+  return array.slice(0, array.length-1)
+  // return array
+}
